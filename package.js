7cd2851d47142173899638008a43ec1266e84eb7
Abigail Watson <abigailwatson@Abigails-Mac-mini-380428.local>
2018-09-09 20:57:50 -0500

    sync

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-list',
-  version: '1.4.1',
+  version: '1.4.2',
   summary: 'HL7 FHIR Resource - List',
   git: 'https://github.com/clinical-meteor/hl7-resource-list',
   documentation: 'README.md'
@@ -35,5 +35,7 @@ Package.onUse(function (api) {
 });
 
 Npm.depends({
-  "simpl-schema": "1.5.3"
+  "simpl-schema": "1.5.3",
+  "moment": "2.22.2",
+  "lodash": "4.17.4"
 })
